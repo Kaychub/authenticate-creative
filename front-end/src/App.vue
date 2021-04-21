be389730a517e5b9b6805050f9f702903c3ef432
Kaychub <kaychubyt@gmail.com>
2021-04-21 14:23:49 -0600

    login box

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
     <img src='/vue-creative-logo.svg' />
-    <div id="nav">
+    <div class='login-info'>
       <div v-if='this.$root.$data.user'>
-        <p>Logged in!</p>
+        <p>Logged in as {{this.$root.$data.user.displayName}}</p>
         <button @click='logout'>Log out</button>
       </div>
+      <div v-else>
+        <router-link to='/'>Log in here</router-link>
+      </div>
+    </div>
+    <div id="nav">
       <div class='select-palette'>
         <router-link to="/Select">Select Palette</router-link>
       </div>
