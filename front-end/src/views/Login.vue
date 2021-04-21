58f5f8f21f2dd79dc55ca3da262d998ad04efd67
Kaychub <kaychubyt@gmail.com>
2021-04-21 14:12:50 -0600

    Renamed Home.vue to Login

---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="home">
+    <div class='reg-login'>
+      <div class='register'>
+        <h2>Register</h2>
+        <div class='names'>
+          <input type='text' v-model='displayName' placeholder="Display Name" />
+        </div>
+        <div class='username-password'>
+          <input type='text' v-model='username' placeholder='Username' />
+          <input type='password' v-model='password' placeholder='Password' />
+        </div>
+        <button @click='register()'>Register</button>
+      </div>
+      <div class='login'>
+        <h2>Login</h2>
+        <div class='username-password'>
+          <input type='text' v-model='usernameLogin' placeholder='Username' />
+          <input type='password' v-model='passwordLogin' placeholder='Password' />
+        </div>
+        <button @click='login()'>Login</button>
+      </div>
+    </div>
+    <div class='error' v-if='this.error || this.errorLogin'>
+      <h2>Something went wrong!</h2>
+      <p v-if='this.error'>{{ this.error }}</p>
+      <p v-if='this.errorLogin'>{{ this.errorLogin }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() { return {
+    error: '',
+    errorLogin: '',
+    displayName: '',
+    username: '',
+    password: '',
+    usernameLogin: '',
+    passwordLogin: '',
+  }},
+  components: {
+  },
+  async created() {
+    try {
+      let response = await axios.get('/api/users');
+      this.$root.$data.user = response.data.user;
+    } catch (error) {
+      this.$root.$data.user = null;
+    }
+  },
+  methods: {
+    async register() {
+      this.error = '';
+      this.errorLogin = '';
+      if (!this.displayName || !this.username || !this.password)
+        return;
+      try {
+        let response = await axios.post('/api/users', {
+          displayName: this.displayName,
+          username: this.username,
+          password: this.password,
+        });
+        this.$root.$data.user = response.data.user;
+        this.displayName = '';
+        this.username = '';
+        this.password = '';
+      } catch (error) {
+        this.error = error.response.data.message;
+        this.$root.$data.user = null;
+      }
+    },
+    async login() {
+      this.error = '';
+      this.errorLogin = '';
+      if (!this.usernameLogin || !this.passwordLogin)
+        return;
+      try {
+        let response = await axios.post('/api/users/login', {
+          username: this.usernameLogin,
+          password: this.passwordLogin,
+        });
+        this.$root.$data.user = response.data.user;
+        this.usernameLogin = '';
+        this.passwordLogin = '';
+      } catch (error) {
+        this.errorLogin = "Error: " + error.response.data.message;
+        this.$root.$data.user = null;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*
+TODO
+-> Add media queries
+--> When screen gets small, make reg/login stack vertical
+*/
+.reg-login {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-size: 18px;
+}
+
+.register, .login {
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 35%;
+
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
