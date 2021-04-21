257af5b4280764190b02ae4b6a997bad0d9248e7
Kaychub <kaychubyt@gmail.com>
2021-04-21 11:42:51 -0600

    Added user to data, removed old mock-data

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import mock from './mock-data.js'
+// import mock from './mock-data.js'
 
 Vue.config.productionTip = false
 
 let data = {
   selectedPaletteID: 0,
-  swatches: mock,
+  user: null,
+  // swatches: mock,
   palette: [],
   userGeneratedID: 1001, // custom id so that user-made swatches don't get mixed up with the mock-data ones
 }
