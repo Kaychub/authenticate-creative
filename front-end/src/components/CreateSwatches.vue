a30490fc1ff54a65496e7b21092e66ac76a9103d
Kaychub <kaychubyt@gmail.com>
2021-04-21 19:36:57 -0600

    Tab formatting

--- front-end/src/components/CreateSwatches.vue
+++ front-end/src/components/CreateSwatches.vue
@@ -28,91 +28,91 @@
 <script>
 import axios from 'axios';
 export default {
-    name: 'CreateSwatches',
-    data: () => {
-        return {
-            selectedPalette: null,
-            badName: false,
-            name: '',
-            colors: [
-                        {
-                            id: 1,
-                            badName: false,
-                            color: '#aa00ff',
-                        },
-                    ],
-        }
-    },
-    async created() {
-        if (this.$root.$data.selectedPaletteID == 0) {
-            return;
-        }
-        try {
-            const response = await axios.get('/api/palettes/' + this.$root.$data.selectedPaletteID);
-            this.selectedPalette = response.data;
-        }  catch (error) {
-            console.log(error); 
-        }
-    },
-    methods: {
-        async addSwatch() {
-            // We only update badName here because we don't want the field to be red until they attempt to submit
-            this.badName = !this.name; // badName = we don't have a name
-
-            // check to make sure each color is appropriate
-            let colorFail = false;
-            for (let c of this.colors) {
-                if (!/^#[0-9a-fA-F]{6}$/.test(c.color)) { // regex: #[hex char]{6 times}
-                    c.badName = true;
-                    colorFail = true;
-                } else {
-                    c.badName = false;
-                }
-            }
-
-            // If there's a bad color name or no swatch name, just return early
-            if (colorFail || this.badName)
-                return;
-            
-            try {
-                await axios.post('/api/palettes/' + this.selectedPalette._id + '/swatches/', {
-                    // The prefab swatches have .added, but that's just a remnant of how we add keep track of which ones we've added
-                    id: this.$root.$data.userGeneratedID++, // increase id at the same time so we don't get duplicate ids
-                    name: this.name,
-                    colors: this.colors.map(el => el.color),
-                });
-                // reset after creation
-                this.badName = false;
-                this.name = '';
-                this.colors = [
-                    {
-                        id: 1,
-                        badName: false,
-                        color: '#aa00ff',
-                    },
-                ];
-            } catch (error) {
-                console.log(error);
-            }
+  name: 'CreateSwatches',
+  data: () => {
+    return {
+      selectedPalette: null,
+      badName: false,
+      name: '',
+      colors: [
+        {
+          id: 1,
+          badName: false,
+          color: '#aa00ff',
         },
-        addColor() {
-            if (this.colors.length > 7)
-                return;
-            this.colors.push({
-                id: this.colors[this.colors.length-1].id+1,
-                badName: false,
-                color: '#' + (Math.floor(Math.random()*0xeeeeee) + 0x111111).toString(16),
-            });
-        },
-        removeColor(color) {
-            if (this.colors.length == 1)
-                return;
-            let index = this.colors.findIndex(el => el.id == color.id);
-            this.colors.splice(index, 1);
-        }
-    },
-    computed: {
+      ],
     }
+  },
+  async created() {
+    if (this.$root.$data.selectedPaletteID == 0) {
+      return;
+    }
+    try {
+      const response = await axios.get('/api/palettes/' + this.$root.$data.selectedPaletteID);
+      this.selectedPalette = response.data;
+    } catch (error) {
+      console.log(error); 
+    }
+  },
+  methods: {
+    async addSwatch() {
+      // We only update badName here because we don't want the field to be red until they attempt to submit
+      this.badName = !this.name; // badName = we don't have a name
+
+      // check to make sure each color is appropriate
+      let colorFail = false;
+      for (let c of this.colors) {
+        if (!/^#[0-9a-fA-F]{6}$/.test(c.color)) { // regex: #[hex char]{6 times}
+          c.badName = true;
+          colorFail = true;
+        } else {
+          c.badName = false;
+        }
+      }
+
+      // If there's a bad color name or no swatch name, just return early
+      if (colorFail || this.badName)
+        return;
+      
+      try {
+        await axios.post('/api/palettes/' + this.selectedPalette._id + '/swatches/', {
+          // The prefab swatches have .added, but that's just a remnant of how we add keep track of which ones we've added
+          id: this.$root.$data.userGeneratedID++, // increase id at the same time so we don't get duplicate ids
+          name: this.name,
+          colors: this.colors.map(el => el.color),
+        });
+        // reset after creation
+        this.badName = false;
+        this.name = '';
+        this.colors = [
+          {
+            id: 1,
+            badName: false,
+            color: '#aa00ff',
+          },
+        ];
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    addColor() {
+      if (this.colors.length > 7)
+        return;
+      this.colors.push({
+        id: this.colors[this.colors.length-1].id+1,
+        badName: false,
+        color: '#' + (Math.floor(Math.random()*0xeeeeee) + 0x111111).toString(16),
+      });
+    },
+    removeColor(color) {
+      if (this.colors.length == 1)
+        return;
+      let index = this.colors.findIndex(el => el.id == color.id);
+      this.colors.splice(index, 1);
+    }
+  },
+  computed: {
+  }
 }
 </script>
 
